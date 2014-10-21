386271f0d5221f58bd51dd4443a5d525492bb693
Steven Wade <stevenmwade@gmail.com>
2014-10-21 14:09:53 -0600

    index scaffold

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
 var indexController = require('./controllers/index.js');
+
+mongoose.connect('mongodb://localhost/lingo');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -9,6 +12,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+app.get('/translate', indexController.translate);
+app.get('/quiz', indexController.quiz);
+app.get('/progress', indexController.progress);
 
 var server = app.listen(9063, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,15 @@
 var indexController = {
 	index: function(req, res) {
 		res.render('index');
+	},
+	translate: function(req, res){
+		res.render('translate');
+	},
+	quiz: function(req, res){
+		res.render('quiz');
+	},
+	progress: function(req, res){
+		res.render('progress');
 	}
 };
 
